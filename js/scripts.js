a846f643eab6b7b37d8a5b741d8c5aa9148f1795
michaelkoceja <michael.koceja@gmail.com>
2017-04-28 10:58:56 -0700

    add more hidden id's to css, add if and else if to js

--- js/scripts.js
+++ js/scripts.js
@@ -9,5 +9,17 @@ $(function() {
       $(".answer").hide();
       $("#css").show();
     }
+    else if (questions1 === "filler1" && questions2 === "filler2") {
+      $(".answer").hide();
+      $("#csharp").show();
+    }
+    else if (questions1 === "filler1" && questions2 === "filler3") {
+      $(".answer").hide();
+      $("#php").show();
+    }
+    else if (questions1 === "filler1" && questions2 === "filler4") {
+      $(".answer").hide();
+      $("#java").show();
+    }
   });
 });
